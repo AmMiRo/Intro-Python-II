5f8c6a09590d9017098feae83caef47d9fff1394
Amos Rose <amosroseacct@gmail.com>
2020-06-03 17:29:11 -0600

    day 2 MVP reached with stretch

--- src/adv.py
+++ src/adv.py
@@ -1,32 +1,35 @@
+# Imports here
 import textwrap
 import site
 from room import Room
 from player import Player
+from item import Item
 
-# Declare all the rooms
-
+# Declare all the rooms with items
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ["food"]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ["sword", "shield"]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ["gold"]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ["armor"]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
+
+    "secret": Room("Secret Chamber", """You've found the secret closet hidden in the
+Treasure Chamber! There is a lone pedestal holding a book in the corner""", ["spell book"]),
 }
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -35,52 +38,71 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room["treasure"].e_to = room["secret"]
+room["secret"].w_to = room["treasure"]
 
-#
-# Main
-#
+# Declare items
+items = {
+    "sword": Item("sword", "is very sharp"),
 
-# Make a new player object that is currently in the 'outside' room.
+    "armor": Item("armor", "is form fitting and makes your butt look great"),
 
-player1 = Player(room["outside"])
+    "shield": Item("shield", "is pretty heavy"),
 
-# Write a loop that:
+    "food": Item("food", "is raw"),
+
+    "gold": Item("gold", "is shiny"),
+
+    "spell book": Item("spell book", "is in a language you don't understand")
+}
+
+
+player = Player(input("What is your name, adventurer?"), room["outside"], [])
+
+print(f"\nHello, {player.name}! Your adventure starts now.")
 
 while True:
-# * Prints the current room name
-    print(f"\n{player1.current_room.name}\n")
-# * Prints the current description (the textwrap module might be useful here). (***might use detent***)
-    print(player1.current_room.description + "\n")
-# * Waits for user input and decides what to do.
-    comand = input("\nWhich way should we go?\n").lower()
-    if comand == "q":
-        print("goodbye\n")
-        exit()
-    elif comand == "n":
-        if player1.current_room.n_to is not None:
-            player1.current_room = player1.current_room.n_to
+
+    print(f"\n{player.current_room.name}:")
+
+    print(player.current_room.description + "\n")
+
+    cmd = input("What will you do? (enter help for options)\n").lower().split(" ")
+    if len(cmd) == 1:
+
+        cmd = cmd[0]
+
+        if cmd == "q":
+            print("\ngoodbye\n")
+            exit()
+
+        elif cmd in ["n", "s", "e", "w"] :
+            player.move_self(cmd)
+
+        elif cmd == "search":
+            player.current_room.search_room()
+
+        elif cmd == "inventory":
+            player.get_inventory()
+        
+        elif cmd == "help":
+            print("Try: n, s, e, or w to move.\nTry: search to find items.\nTry: inspect, get, drop, or inventory with items.\n")
+
         else:
-            print("\nYou cannot go that way\n")
-    elif comand == "s":
-        if player1.current_room.s_to is not None:
-            player1.current_room = player1.current_room.s_to
-        else:
-            print("\nYou cannot go that way\n")
-    elif comand == "e":
-        if player1.current_room.e_to is not None:
-            player1.current_room = player1.current_room.e_to
-        else:
-            print("\nYou cannot go that way\n")
-    elif comand == "w":
-        if player1.current_room.w_to is not None:
-            player1.current_room = player1.current_room.w_to
-        else:
-            print("\nYou cannot go that way\n")
+            print("\nI don't understand that command.\n")
+
+    elif len(cmd) == 2:
+
+        item = cmd[1]
+
+        if cmd[0] == "get":
+            player.get_item(item)
+
+        elif cmd[0] == "inspect":
+            items[item].inspect_item(player, item)
+
+        elif cmd[0] == "drop":
+            player.drop_item(item)
+            
     else:
-        print("\nI don't understand that command\n")
-    
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+        print("\nI don't understand that command.\n")
--- src/room.py
+++ src/room.py
@@ -2,10 +2,19 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.items = items
+    
+    def search_room(self):
+        if self.items:
+            print("You found:")
+            for item in self.items:
+                print(item)
+        else:
+            print("You found nothing.\n")
